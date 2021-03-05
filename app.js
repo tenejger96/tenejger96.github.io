c0e7dde111e295d251b9b11758621f74553e9fb1
tenejger96 <59974124+tenejger96@users.noreply.github.com>
2021-03-05 14:27:06 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+var sudokuApp = new Vue({
+
+    el: '#app-sudoku',
+
+    data: {
+
+        sudokuMatrix: [],
+        initializeGameText: "Погнали!",
+        evaluateGameText: "Проверить!",
+        answerImage: "",
+        isGameStarted: false,
+        showAnswer: false
+    },
+
+    methods: {
+
+        initializeGame() {
+
+            var defaultSudokuMatrix = [
+                [{ num: 5 }, { num: 3 }, { num: 4 }, { num: 6 }, { num: 7 }, { num: 8 }, { num: 9 }, { num: 1 }, { num: 2 }],
+                [{ num: 6 }, { num: 7 }, { num: 2 }, { num: 1 }, { num: 9 }, { num: 5 }, { num: 3 }, { num: 4 }, { num: 8 }],
+                [{ num: 1 }, { num: 9 }, { num: 8 }, { num: 3 }, { num: 4 }, { num: 2 }, { num: 5 }, { num: 6 }, { num: 7 }],
+                [{ num: 8 }, { num: 5 }, { num: 9 }, { num: 7 }, { num: 6 }, { num: 1 }, { num: 4 }, { num: 2 }, { num: 3 }],
+                [{ num: 4 }, { num: 2 }, { num: 6 }, { num: 8 }, { num: 5 }, { num: 3 }, { num: 7 }, { num: 9 }, { num: 1 }],
+                [{ num: 7 }, { num: 1 }, { num: 3 }, { num: 9 }, { num: 2 }, { num: 4 }, { num: 8 }, { num: 5 }, { num: 6 }],
+                [{ num: 9 }, { num: 6 }, { num: 1 }, { num: 5 }, { num: 3 }, { num: 7 }, { num: 2 }, { num: 8 }, { num: 4 }],
+                [{ num: 2 }, { num: 8 }, { num: 7 }, { num: 4 }, { num: 1 }, { num: 9 }, { num: 6 }, { num: 3 }, { num: 5 }],
+                [{ num: 3 }, { num: 4 }, { num: 5 }, { num: 2 }, { num: 8 }, { num: 6 }, { num: 1 }, { num: 7 }, { num: 9 }]
+            ];
+
+            // Empty two random cells per row
+            for (var i = 0; i < defaultSudokuMatrix.length; ++i) {
+                for (var k = 0; k < 2; ++k) {
+                    var randomColumnIndex = Math.floor(Math.random() * defaultSudokuMatrix.length);
+                    defaultSudokuMatrix[i][randomColumnIndex].num = "";
+                }
+            }
+
+            this.sudokuMatrix = defaultSudokuMatrix;
+            this.initializeGameText = "Заново";
+            this.isGameStarted = true;
+        },
+
+        evaluateGame() {
+
+            var copyOfSudokuMatrix = [];
+            for (var i = 0; i < this.sudokuMatrix.length; ++i) {
+
+                if (!copyOfSudokuMatrix[i])
+                    copyOfSudokuMatrix[i] = [];
+
+                for (var k = 0; k < this.sudokuMatrix[i].length; ++k) {
+                    copyOfSudokuMatrix[i][k] = this.sudokuMatrix[i][k].num;
+                }
+
+            }
+
+            var sudokuSolver = Sudoku.init(copyOfSudokuMatrix);
+
+            if (sudokuSolver.isValid()) {
+
+                this.answerImage = "success.gif";
+                this.showAnswer = true;
+                this.isGameStarted = false;
+
+                setTimeout(() => {
+                    this.showAnswer = false;
+                    this.isGameStarted = true;
+                }, 60000);
+
+            }
+            else {
+
+                this.answerImage = "fail.gif";
+                this.showAnswer = true;
+                this.isGameStarted = false;
+
+                setTimeout(() => {
+                    this.showAnswer = false;
+                    this.isGameStarted = true;
+                }, 5000);
+
+            }
+
+        }
+
+    }
+});
